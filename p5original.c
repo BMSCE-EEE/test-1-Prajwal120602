716e1374c3b43e01d9ae4d1843416fb1331b5d74
Prajwal120602 <94945104+Prajwal120602@users.noreply.github.com>
2022-02-08 14:10:42 +0000

    smallest number pass by value

---
+++ p5original.c
@@ -0,0 +1,42 @@
+//compare 3 number,use 4 function 
+//pass by value
+
+#include <stdio.h>
+void input();
+void smallest(int ,int ,int);
+void output();
+ int a,b,c,r;
+int main()
+{
+    input();
+    smallest(a,b,c);
+    output();
+    return 0;
+}
+void input()
+{
+    
+     printf("enter the three number\n");
+     scanf("%d %d %d",&a,&b,&c);
+}
+void smallest(int x,int y,int z)
+{
+    
+    if(x<y && x<z)
+    {
+        r=x;
+    }
+    else if(y<z)
+    {
+        r=y;
+    }
+    else
+    {
+        r=z;
+    }
+}
+void output()
+{
+    printf("smallest number is=%d",r);
+   
+}
